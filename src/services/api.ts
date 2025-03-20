213fe14b2ae0459f983e8bd92c6385d16fbd7274
Rodrigo Lopes <tecnico32@villela.local>
2025-03-20 10:59:09 -0300

    Versão Funcional troca de rota API(Back-end)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL base da API
-export const API_URL = 'https://ville5113.c44.integrator.host/api';
+export const API_URL = 'http://localhost:3001/api';
 // URL base do frontend
 export const FRONTEND_URL = 'https://ville5113.c44.integrator.host/';
 
